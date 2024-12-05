Fixed reliance on labels for attention mask

src/data/NERDataset.py
@@ -73,7 +73,7 @@ class NERDataset(Dataset):
 
         batch_data = torch.tensor(batch_data, dtype=torch.long)
         batch_labels = torch.tensor(batch_labels, dtype=torch.long)
-        batch_attention_mask = (batch_labels != -100).long()
+        batch_attention_mask = (batch_data != self.word_pad_idx).long()
 
         batch_data = batch_data.to(self.device)
         batch_labels = batch_labels.to(self.device)

src/main.py
@@ -1,9 +1,13 @@
+import torch
 from data.LangDataloader import LanguageDataLoader
 
-from models.BertBilstmCrf import BERTBiLSTMCRF
+from data.NERDataloader import create_dataloaders
+from models.XLMRobertaBilstmCrf import XLMRoBERTaBiLSTMCRF
 
 from utils.logging import logger
 from utils.load_config import load_config
+
+from train_eval import predict_test
 
 # Load configuration
 config = load_config()
@@ -15,7 +19,33 @@ DEVICE = config["model"]["device"]
 logger.info("Downloading data...")
 dataloader = LanguageDataLoader(config)
 language_data = dataloader.load_language_groups()
-print(language_data)
 
 # Define model
-model = BERTBiLSTMCRF
+NUM_LABELS = config["model"]["num_labels"]
+DEVICE = config["model"]["device"]
+
+model = XLMRoBERTaBiLSTMCRF(NUM_LABELS).to(DEVICE)
+model.load_state_dict(torch.load("src/models/pretrained/best_germanic.pth", weights_only=True))
+
+train_dataloader, test_dataloader, val_dataloader = create_dataloaders(language_data["germanic"], config, use_transfer_learning=False)
+
+emissions, labels = predict_test(model, train_dataloader)
+predictions = emissions.argmax(dim=-1)
+print("SHAPE: ", emissions.shape)
+print("PREDS SHAPE: ", predictions.shape)
+
+id2label = config["data"]["id2label"]
+batch_labels = [
+    [id2label[idx.item()] for idx in sequence] 
+    for sequence in predictions
+]
+
+actual_labels = [
+    [id2label[idx.item()] for idx in sequence if idx != -100] 
+    for sequence in labels
+]
+
+for thing, thing2 in zip(batch_labels, actual_labels):
+    print("BATCH LABELS: ", thing)
+    print("ACTUAL: ", thing2)
+    print()

src/models/BertBilstmCrf.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 from transformers import BertModel
 from torchcrf import CRF
@@ -27,7 +28,10 @@ class BERTBiLSTMCRF(nn.Module):
         return emissions
 
     def loss(self, emissions, tags, mask):
-        return -self.crf(emissions, tags, mask=mask)
+
+        adjusted_tags = torch.where(tags == -100, torch.tensor(0, device=tags.device), tags)
+
+        return -self.crf(emissions, adjusted_tags, mask=mask)
 
     def decode(self, emissions, mask):
         return self.crf.decode(emissions, mask=mask)

src/train_eval.py
@@ -65,11 +65,9 @@ def train_epoch(model, dataloader, optimizer, device):
     total_loss = 0.0
     total_f1 = 0.0
 
-    zero_tensor = torch.tensor(0, device=device)
-
     for input_ids, labels, attention_mask in dataloader:
-        # Replace -100 in labels with 0 (or any valid tag) temporarily for CRF computation
-        labels = torch.where(labels == model.label_pad_idx, zero_tensor, labels)
+        # Esnure mask ignores padding labels
+        #adjusted_mask = attention_mask & (labels != model.label_pad_idx)
 
         # Forward pass
         optimizer.zero_grad(set_to_none=True)
@@ -100,12 +98,16 @@ def train_epoch(model, dataloader, optimizer, device):
 def evaluate_epoch(model, dataloader, device):
     model.eval()
     total_f1 = 0.0
-    zero_tensor = torch.tensor(0, device=device)
+    
+    dummy_label = model.num_tags
+    dummy_tensor = torch.tensor(dummy_label, device=device)
 
     with torch.no_grad():
         for input_ids, labels, attention_mask in dataloader:
             # Replace -100 in labels with 0 (or any valid tag) temporarily for CRF computation
-            labels = torch.where(labels == model.label_pad_idx, zero_tensor, labels)
+            # labels = torch.where(labels == model.label_pad_idx, dummy_tensor, labels)
+
+            #adjusted_mask = attention_mask & (labels != -100)
 
             # Forward pass
             emissions = model(input_ids, attention_mask)
@@ -120,3 +122,12 @@ def evaluate_epoch(model, dataloader, device):
     avg_f1 = total_f1 / len(dataloader)
 
     return avg_f1
+
+
+def predict_test(model, dataloader):
+    for input_ids, _, attention_mask in dataloader:
+        print(input_ids.shape)
+        # Forward pass
+        emissions = model(input_ids, attention_mask)
+        print(emissions.shape)
+        return emissions

src/utils/metrics.py
@@ -10,7 +10,7 @@ def f1_score(masked_predictions, masked_labels, num_labels):
     return f1_score_tensor.cpu().numpy().item()
 
 
-def prepare_labels(emissions, labels, attention_mask):
+def prepare_labels(emissions, labels, attention_mask, label_pad_idx=-100):
     # Prepare outputs for F1-scoring
     predicted_labels = emissions.argmax(dim=-1)
 
@@ -19,7 +19,7 @@ def prepare_labels(emissions, labels, attention_mask):
     labels = torch.flatten(labels)
     attention_mask = torch.flatten(attention_mask)
 
-    mask = attention_mask == 1
+    mask = (attention_mask == 1) & (labels != label_pad_idx)
 
     masked_predictions = predicted_labels[mask]
     masked_labels = labels[mask]