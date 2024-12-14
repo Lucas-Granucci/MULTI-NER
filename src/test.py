331b465862194b89c5290c6182d0169cc6a2b2fd
Lucas Granucci <lucasgranucci08@gmail.com>
2024-12-13 20:47:49 -0600

    Changed to new language

---
+++ src/test.py
@@ -0,0 +1,19 @@
+from data.LangDataloader import LanguageDataLoader
+from data.DataCreator import DataCreator
+from utils.load_config import load_config
+
+# Load configuration
+config = load_config()
+
+# Instantiate data creator
+dataCreator = DataCreator()
+
+dataloader = LanguageDataLoader(config)
+language_data = dataloader.load_language_groups()
+
+
+dataCreator.translate_text_data(
+    language_data["germanic"]["high_resource"]["test"],
+    "Asturian",
+    "asturian.txt",
+)
